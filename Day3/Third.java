b10ff33a4468ae058d2b45ca7543de43e1f2076c
KASHISH GUPTA <143589321+Kritya9935@users.noreply.github.com>
2025-01-23 16:15:23 +0530

    Add files via upload

---
+++ Day3/Third.java
@@ -0,0 +1,15 @@
+//write a program to calculate and print the percentage of total marks where student has scored 430 marks out of 500
+
+class Third
+{
+public static void main(String args[])
+{
+int tmarks =500;
+int scoredMarks = 430;
+int percentage;
+percentage = (scoredMarks * 100)/tmarks;
+
+System.out.println("percentage = "+percentage+"%");
+}
+
+}
