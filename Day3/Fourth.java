ca4bb5c2de75fb324addb65e7405d69cf45821dc
KASHISH GUPTA <143589321+Kritya9935@users.noreply.github.com>
2025-01-23 16:14:58 +0530

    Add files via upload

---
+++ Day3/Fourth.java
@@ -0,0 +1,13 @@
+//write a program to calculate and print the area of circle where the value of pi = 3.14 and radius(input)
+
+
+class Fourth 
+{
+	public static void main(String args[])
+	{
+ 		float pi = 3.14f;
+		float r = 3.5f;
+		float area = pi*r*r;
+		System.out.println("Area of Circle :"+area);
+	}
+}
